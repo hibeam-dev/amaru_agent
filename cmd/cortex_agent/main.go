663b8e5a7380773ffa7683b249093f647df82172
Ricardo Lanziano <arpunk@fatelectron.net>
2025-04-23 17:37:07 -0500

    Include full i18n support
    Closes #9
    
    Signed-off-by: Ricardo Lanziano <arpunk@fatelectron.net>

--- cmd/cortex_agent/main.go
+++ cmd/cortex_agent/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"flag"
+	"fmt"
 	"log"
 
 	"erlang-solutions.com/cortex_agent/internal/app"
@@ -12,19 +13,33 @@ import (
 )
 
 var (
-	jsonMode   = flag.Bool("json", false, "Enable JSON communication mode for application integration")
-	writePid   = flag.Bool("pid", false, "Write PID file to ~/.cortex_agent/cortex_agent.pid")
-	configFile = flag.String("config", "config.toml", "Path to TOML configuration file")
+	jsonMode   = flag.Bool("json", false, "")
+	writePid   = flag.Bool("pid", false, "")
+	configFile = flag.String("config", "config.toml", "")
 )
 
+func init() {
+	flag.Usage = func() {
+		w := flag.CommandLine.Output()
+		_, _ = fmt.Fprintf(w, "%s\n\n", i18n.T("usage_header", nil))
+		_, _ = fmt.Fprintf(w, "%s\n", i18n.T("usage_format", nil))
+		_, _ = fmt.Fprintf(w, "\n%s\n", i18n.T("options_header", nil))
+
+		_, _ = fmt.Fprintf(w, "  -json\n    \t%s\n", i18n.T("flag_json_desc", nil))
+		_, _ = fmt.Fprintf(w, "  -pid\n    \t%s\n", i18n.T("flag_pid_desc", nil))
+		_, _ = fmt.Fprintf(w, "  -config %s\n    \t%s\n", "filename", i18n.T("flag_config_desc", nil))
+	}
+}
+
 func main() {
+	if err := i18n.InitDefaultFS(); err != nil {
+		// Can't use i18n.T here since i18n init failed
+		log.Printf("Warning: Failed to initialize localization: %v", err)
+	}
+
 	flag.Parse()
 
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-
-	if err := i18n.InitDefaultFS(); err != nil {
-		log.Printf(i18n.Tf("i18n_init_error", nil), i18n.T("i18n_init_error", map[string]interface{}{"Error": err}))
-	}
 
 	if *writePid {
 		if err := daemon.WritePidFile(); err != nil {
