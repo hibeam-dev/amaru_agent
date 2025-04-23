9004d8bfd8c9d98124558be617c337c52d1c3a92
Ricardo Lanziano <arpunk@fatelectron.net>
2025-04-23 17:39:05 -0500

    Include full i18n support (#10)
    Closes #9
    
    Signed-off-by: Ricardo Lanziano <arpunk@fatelectron.net>

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -12,19 +12,19 @@ import (
 func WritePidFile() error {
 	home, err := os.UserHomeDir()
 	if err != nil {
-		return fmt.Errorf("unable to determine user home directory: %w", err)
+		return fmt.Errorf("%s: %w", i18n.T("home_dir_error", nil), err)
 	}
 
 	pidDir := filepath.Join(home, ".cortex_agent")
 	if err := os.MkdirAll(pidDir, 0755); err != nil {
-		return fmt.Errorf("failed to create PID directory: %w", err)
+		return fmt.Errorf("%s: %w", i18n.T("pid_dir_create_error", nil), err)
 	}
 
 	pidPath := filepath.Join(pidDir, "cortex_agent.pid")
 	pid := os.Getpid()
 
 	if err := os.WriteFile(pidPath, []byte(fmt.Sprintf("%d", pid)), 0644); err != nil {
-		return fmt.Errorf("failed to write PID file: %w", err)
+		return fmt.Errorf("%s: %w", i18n.T("pid_file_write_error", nil), err)
 	}
 
 	log.Printf(i18n.Tf("pid_file_written", nil),
