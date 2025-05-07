036edde6bc0dfbf731783d852559f60c0389adaf
Ricardo Lanziano <arpunk@fatelectron.net>
2025-05-07 00:50:31 -0500

    Send local IP in the application configuration
    Closes #46
    
    Signed-off-by: Ricardo Lanziano <arpunk@fatelectron.net>

--- internal/transport/config.go
+++ internal/transport/config.go
@@ -12,6 +12,7 @@ type ConfigPayload struct {
 type ApplicationConfig struct {
 	Hostname string            `json:"hostname"`
 	Port     int               `json:"port"`
+	IP       string            `json:"ip,omitempty"`
 	Tags     map[string]string `json:"tags"`
 	Security map[string]bool   `json:"security"`
 	Tunnel   bool              `json:"tunnel"`
