53ad84c59c902f17146c4cf4926b94401f2916b3
Ricardo Lanziano <arpunk@fatelectron.net>
2025-09-22 12:07:40 -0500

    Store suggested configuration from the backend (#86)
    Closes #83
    
    Signed-off-by: Ricardo Lanziano <arpunk@fatelectron.net>

--- internal/register/register.go
+++ internal/register/register.go
@@ -13,7 +13,7 @@ import (
 )
 
 type Client interface {
-	RegisterWithBackend(token string, publicKeyPath string, backendHost string, hostname string) error
+	RegisterWithBackend(token string, publicKeyPath string, backendHost string, hostname string) ([]byte, error)
 }
 
 type client struct{}
@@ -22,10 +22,10 @@ func NewClient() Client {
 	return &client{}
 }
 
-func (c *client) RegisterWithBackend(token string, publicKeyPath string, backendHost string, hostname string) error {
+func (c *client) RegisterWithBackend(token string, publicKeyPath string, backendHost string, hostname string) ([]byte, error) {
 	publicKey, err := readPublicKey(publicKeyPath)
 	if err != nil {
-		return fmt.Errorf("%s", i18n.T("register_key_read_error", map[string]any{"Error": err}))
+		return nil, fmt.Errorf("%s", i18n.T("register_key_read_error", map[string]any{"Error": err}))
 	}
 
 	registrationData := map[string]string{
@@ -39,13 +39,13 @@ func (c *client) RegisterWithBackend(token string, publicKeyPath string, backend
 
 	jsonData, err := json.Marshal(registrationData)
 	if err != nil {
-		return fmt.Errorf("%s", i18n.T("register_json_error", map[string]any{"Error": err}))
+		return nil, fmt.Errorf("%s", i18n.T("register_json_error", map[string]any{"Error": err}))
 	}
 
 	registrationURL := backendHost + "/api/v1/agent/register"
 	resp, err := http.Post(registrationURL, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
-		return fmt.Errorf("%s", i18n.T("register_request_error", map[string]any{"Error": err}))
+		return nil, fmt.Errorf("%s", i18n.T("register_request_error", map[string]any{"Error": err}))
 	}
 	defer func() {
 		if closeErr := resp.Body.Close(); closeErr != nil {
@@ -55,14 +55,14 @@ func (c *client) RegisterWithBackend(token string, publicKeyPath string, backend
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return fmt.Errorf("%s", i18n.T("register_response_read_error", map[string]any{"Error": err}))
+		return nil, fmt.Errorf("%s", i18n.T("register_response_read_error", map[string]any{"Error": err}))
 	}
 
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
-		return fmt.Errorf("%s", i18n.T("register_response_error", map[string]any{"StatusCode": resp.StatusCode, "Body": string(body)}))
+		return nil, fmt.Errorf("%s", i18n.T("register_response_error", map[string]any{"StatusCode": resp.StatusCode, "Body": string(body)}))
 	}
 
-	return nil
+	return body, nil
 }
 
 func readPublicKey(keyPath string) ([]byte, error) {
